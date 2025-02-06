5514142aa28a61e04e2df08f372c75d5efbcdf54
antonwessel <anton62045@gmail.com>
2025-02-06 11:23:28 +0100

    Make 2 mock users

--- ClassLibrary/Core/Helpers/UserMockData.cs
+++ ClassLibrary/Core/Helpers/UserMockData.cs
@@ -1,5 +1,10 @@
-﻿namespace ClassLibrary.Core.Helpers;
+﻿using ClassLibrary.Core.Models;
+
+namespace ClassLibrary.Core.Helpers;
 
 public class UserMockData
 {
+    private static readonly List<User> _users = [new User("bob@gmail.com", "123"), new User("john@gmail.com", "4321")];
+
+    public static List<User> GetUsers() => _users;
 }
