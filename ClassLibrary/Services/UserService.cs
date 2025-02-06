54b3eae349452fa5af5f2cce315d417b1a899672
antonwessel <anton62045@gmail.com>
2025-02-06 11:26:51 +0100

    Get users

--- ClassLibrary/Services/UserService.cs
+++ ClassLibrary/Services/UserService.cs
@@ -1,5 +1,17 @@
-﻿namespace ClassLibrary.Services;
+﻿using ClassLibrary.Core.Helpers;
+using ClassLibrary.Core.Models;
+
+namespace ClassLibrary.Services;
 
 public class UserService
 {
+    public List<User> AllUsers { get; set; }
+
+    public UserService()
+    {
+        AllUsers = UserMockData.GetUsers();
+    }
+
+    public List<User> GetUsers() => AllUsers;
+    
 }
