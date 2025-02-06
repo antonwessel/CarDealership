e22f1713f8c22b618668f93beab46669d6349714
frederikG1 <kendamaftw123@gmail.com>
2025-02-06 11:05:18 +0100

    LoginWebsite

---
+++ CarDealership/Pages/LoginWebsite.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CarDealership.Pages
+{
+    public class LoginWebsite : PageModel
+    {
+        private readonly ILogger<LoginWebsite> _logger;
+        [BindProperty]
+        public string Email { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        public bool IsLoggedIn { get; set; }
+
+        public LoginWebsite(ILogger<LoginWebsite> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public void LogIn()
+        {
+            if (Email == null) throw new ArgumentNullException("Email is required");
+            if (Password == null) throw new ArgumentNullException("Password is required");
+            else
+            {
+                IsLoggedIn = true;
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            LogIn();
+            return RedirectToPage("LoginWebsite");
+        }
+    }
+}
