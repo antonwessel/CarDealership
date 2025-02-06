a37e374fe4f5344cce2e242e707388086ad7e846
antonwessel <anton62045@gmail.com>
2025-02-06 11:58:40 +0100

    Modify LoginWebsite code-behind

--- CarDealership/Pages/LoginWebsite.cshtml.cs
+++ CarDealership/Pages/LoginWebsite.cshtml.cs
@@ -1,46 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ClassLibrary.Core.Models;
+using ClassLibrary.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace CarDealership.Pages
+namespace CarDealership.Pages;
+
+public class LoginWebsite : PageModel
 {
-    public class LoginWebsite : PageModel
+    private readonly UserService _userService;
+
+    public List<User> Users { get; set; }
+
+    public LoginWebsite(UserService userService)
     {
-        private readonly ILogger<LoginWebsite> _logger;
-        [BindProperty]
-        public string Email { get; set; }
-        [BindProperty]
-        public string Password { get; set; }
-        public bool IsLoggedIn { get; set; }
+        _userService = userService;
+    }
 
-        public LoginWebsite(ILogger<LoginWebsite> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-            
-        }
-
-        public void LogIn()
-        {
-            if (string.IsNullOrEmpty(Email)) throw new ArgumentNullException("Email is required");
-            if (string.IsNullOrEmpty(Password)) throw new ArgumentNullException("Password is required");
-            else
-            {
-                IsLoggedIn = true;
-            }
-        }
-
-        public IActionResult OnPost()
-        {
-            LogIn();
-            return RedirectToPage("LoginWebsite");
-        }
+    public void OnGet()
+    {
+        Users = _userService.GetUsers();
     }
 }
--- CarDealership/Program.cs
+++ CarDealership/Program.cs
@@ -1,7 +1,10 @@
+using ClassLibrary.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSingleton<UserService, UserService>();
 
 var app = builder.Build();
 
