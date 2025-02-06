0b8c7418bb030d5abdec4d6a3653c8d0e91084b8
antonwessel <anton62045@gmail.com>
2025-02-06 12:54:24 +0100

    Show name of login

--- ClassLibrary/Core/Helpers/UserMockData.cs
+++ ClassLibrary/Core/Helpers/UserMockData.cs
@@ -4,7 +4,7 @@ namespace ClassLibrary.Core.Helpers;
 
 public class UserMockData
 {
-    private static readonly List<User> _users = [new User("bob@gmail.com", "123"), new User("john@gmail.com", "4321")];
+    private static readonly List<User> _users = [new User("bob@gmail.com", "123", "Bob"), new User("john@gmail.com", "4321", "John")];
 
     public static List<User> GetUsers() => _users;
 }
--- ClassLibrary/Core/Models/User.cs
+++ ClassLibrary/Core/Models/User.cs
@@ -4,11 +4,13 @@ public class User
 {
     public string Email { get; set; }
     public string Password { get; set; }
+    public string Name { get; set; }
 
-    public User(string email, string password)
+    public User(string email, string password, string name)
     {
         Email = email;
         Password = password;
+        Name = name;
     }
 
     public User()
